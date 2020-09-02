a7bbce072cccd8164d6241cd5bcb20816edc9ef8
matheus <matheuslima20111997@gmail.com>
2020-09-01 22:45:19 -0300

    app

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ const app = express()
 
 
 app.get('/',(req,res)=>{
-    res.send('asjsaj')
+    res.send('Aba principal')
+})
+
+app.get('/sobre',(req,res)=>{
+    res.send('aba sobre')
+})
+
+app.get('/ola/:nome/:profissao',(req,res)=>{
+    res.send("oi "+req.params.profissao+", "+req.params.nome)
 })
 
 app.listen(3000,function(){
