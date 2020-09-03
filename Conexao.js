cf74ff56d44fd64829ef7631cd73921100a1a6c9
matheus <matheuslima20111997@gmail.com>
2020-09-02 21:53:29 -0300

    con

--- Conexao.js
+++ Conexao.js
@@ -1,15 +1,39 @@
 const Sequelize = require('sequelize')
 
-const con = new Sequelize('sistemadecadastros','root','1234',{
+const con = new Sequelize('dbpostador','root','1234',{
     host:'localhost',
     dialect:'mysql'
 })
 
-con.authenticate()
+/*con.authenticate()
                  .then(function(){
                     console.log('conectou com sucesso')
                  })
                  .catch(function(e){
                     console.log('deu ruim'+e)
                  })               
-              
+              */
+
+const Post = con.define('postagens',{
+    titulo:{
+        type:Sequelize.STRING
+    },
+    conteudo:{
+        type:Sequelize.TEXT
+    }
+})            
+
+const User = con.define('usuarios',{
+    nome:{
+        type:Sequelize.STRING
+    },
+    sexo:{
+        type:Sequelize.CHAR
+    },
+    dtNasc:{
+        type:Sequelize.DATE
+    },
+    telefone:{
+        type:Sequelize.STRING
+    }
+})
